f67d97cbbe87895fafa76867dc7f71aab3c1d690
Wissam LEFEVRE <wissam.lefevre@etudiant.univ-lille1.fr>
2016-03-23 11:30:31 +0100

    "Android"

---
+++ Android/src/eu/battlekart/Course.java
@@ -0,0 +1,17 @@
+package eu.battlekart;
+
+/**
+ * Created by lefevrew on 23/03/16.
+ */
+public class Course {
+    private int id;
+    private String nom;
+
+    public Course(){}
+
+    public Course(int id,String nom){
+        this.id = id;
+        this.nom = nom;
+    }
+
+}
