959208942f9991d18bc56c35a2987c0e9333dc6a
Quentin Brault <tititesouris@laposte.net>
2016-03-23 11:29:55 +0100

    Test fetch from REST

--- Android/src/eu/battlekart/MainActivity.java
+++ Android/src/eu/battlekart/MainActivity.java
@@ -2,6 +2,12 @@ package eu.battlekart;
 
 import android.app.Activity;
 import android.os.Bundle;
+import com.frenchcomputerguy.rest.PostRequest;
+import com.frenchcomputerguy.rest.Request;
+import org.json.JSONException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends Activity {
     /**
@@ -11,5 +17,13 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        Map<String, String> parameters = new HashMap<>();
+        parameters.put("Test", "This is a test");
+        Request post = new PostRequest("http://httpbin.org/post", parameters);
+        try {
+            post.getResponse().getJSONObject().getJSONObject("headers").getString("Test");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
